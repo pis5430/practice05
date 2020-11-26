f12c89fc77b121fa70210641afc8d532201325f5
pyo i seul <isl1089@naver.com>
2020-11-26 19:18:14 +0900

    11/26 과제 7번

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -10,9 +10,41 @@ public class Friend {
     // getter/setter 작성
     
     
-    
-    public void showInfo(){
-        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
-    }
 
+
+ 	public String getName() {
+ 		return name;
+ 	}
+
+
+ 	public void setName(String name) {
+ 		this.name = name;
+ 	}
+
+
+ 	public String getHp() {
+ 		return hp;
+ 	}
+
+
+ 	public void setHp(String hp) {
+ 		this.hp = hp;
+ 	}
+
+
+ 	public String getSchool() {
+ 		return school;
+ 	}
+
+
+ 	public void setSchool(String school) {
+ 		this.school = school;
+ 	}
+
+ 	//일반메소드
+ 	
+     public void showInfo(){
+         System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
+     }
+ 	
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -6,27 +6,29 @@ public class FriendApp {
 
     public static void main(String[] args) {
 
-        Friend[] friendArray = new Friend[3];
+        Friend[] friendArray = new Friend[3]; // Friend 는 주소, 친구 3명등록
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("친구를 3명 등록해 주세요");
 
-        //친구정보 3명 입력 로직 --> 반복문 사용
+        //친구정보 3명 입력 로직 --> 반복문 사용 --> i값으로 가능한지
             
-        	// 친구정보(이름) 입력받기
+        	// 친구정보(이름) 입력받기 String name = sc.nextIine(); 
 
-        	// 친구정보(핸드폰) 입력받기
+        	// 친구정보(핸드폰) 입력받기 String hp = sc.nextIine(); 
         
-        	// 친구정보(학교) 입력받기
+        	// 친구정보(학교) 입력받기 String school = sc.nextIine(); 
 
-            // Friend 인스턴스 생성하여 데이터 넣기
+            // Friend 인스턴스 생성하여 데이터 넣기 p1.getName();
 
-            // 인스턴스 주소를 배열에 대입
+            // 인스턴스 주소를 배열에 대입  -->friendArray[0] = p1; --> p1주소
 
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
+        	//friendArray[i].showInfo();
         }
 
         sc.close();
