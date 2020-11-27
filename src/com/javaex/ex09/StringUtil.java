79e85b6f9f9fc0994ff08619cbd4eb0587432073
pyo i seul <isl1089@naver.com>
2020-11-27 10:54:31 +0900

    11/27 3st commit

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,9 +2,20 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
+    public static String concatString(String[] strArray){
        
-        //메소드 내용작성
+        //메소드 내용작성  -- >반복문으로 배열값이 반복되게
+    	
+    	String total = ""; //초기값 설정해줘야됨
+    	
+    	for(int i=0; i<strArray.length; i++ ) {
+    		
+    		total += strArray[i];
+    	}
+    	
+    	return total;
+    	
+    	
         
     }
 
