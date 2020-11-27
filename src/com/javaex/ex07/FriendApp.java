d72a6f45964e361b7943a8a61993640be9628fa1
pyo i seul <isl1089@naver.com>
2020-11-27 10:03:36 +0900

    11/27 1st commit

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -12,23 +12,39 @@ public class FriendApp {
 
         System.out.println("친구를 3명 등록해 주세요");
 
-        //친구정보 3명 입력 로직 --> 반복문 사용 --> i값으로 가능한지
+//친구정보 3명 입력 로직 --> 반복문 사용 
+        
+        for(int i=0; i<friendArray.length; i++) {
             
         	// 친구정보(이름) 입력받기 String name = sc.nextIine(); 
+        	System.out.print("이름: ");
+        	String name = sc.nextLine();
 
         	// 친구정보(핸드폰) 입력받기 String hp = sc.nextIine(); 
+        	System.out.print("핸드폰: ");
+        	String hp = sc.nextLine();
         
         	// 친구정보(학교) 입력받기 String school = sc.nextIine(); 
+        	System.out.print("학교: ");
+        	String school = sc.nextLine();
 
-            // Friend 인스턴스 생성하여 데이터 넣기 p1.getName();
+            // Friend 인스턴스 생성하여 데이터 넣기 
+        	
+        	Friend people = new Friend(); 
+        	
+        	people.setName(name);
+        	people.setHp(hp);
+        	people.setSchool(school);
 
-            // 인스턴스 주소를 배열에 대입  -->friendArray[0] = p1; --> p1주소
-
+            // 인스턴스 주소를 배열에 대입  -->friendArray[0] = people; --> people주소
+        	
+        	friendArray[i] = people;
+        }
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
-        	//friendArray[i].showInfo();
+        	friendArray[i].showInfo();
         }
 
         sc.close();
